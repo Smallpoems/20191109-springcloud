88c53899cb37d0a9cf17a6e0bc9313118f7970dd
2398586425 <57249526+2398586425@users.noreply.github.com>
2019-11-14 22:52:55 +0800

    springcloud学习项目

--- 20191114-springcloud-config-client-3081/src/main/java/com/aaa/lee/springcloud/ApplicationRun3081.java
+++ 20191114-springcloud-config-client-3081/src/main/java/com/aaa/lee/springcloud/ApplicationRun3081.java
@@ -1,19 +1,14 @@
 package com.aaa.lee.springcloud;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+
 
 @SpringBootApplication
-@EnableConfigServer
-public class ApplicationRun4081 {
-
+public class ApplicationRun3081 {
     public static void main(String[] args) {
 
-        SpringApplication.run(ApplicationRun4081.class,args);
+        SpringApplication.run(ApplicationRun3081.class, args);
     }
-
-
 
 }
